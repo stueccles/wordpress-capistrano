641bba56b726079f702c59978be38ec5cea651bb
Jesse Newland <jnewland@gmail.com>
2008-11-17 20:44:49 -0500

    update capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 #the domain name for the server you'll be running wordpress on
-set :server, "localhost"
+set :domain, "localhost"
 
 #the name of this wordpress project
 set :application, "wordpress-capistrano-test"
@@ -22,14 +22,27 @@ set :git_enable_submodules, 1
 
 #allow deploys w/o having git installed locally
 set(:real_revision) { capture("git ls-remote #{repository} #{branch} | cut -f 1") }
+#no need for system, log, and pids directory
+set :shared_children, %w()
 
-role :app, server
-role :web, server
-role :db,  server, :primary => true
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
 
 namespace :deploy do
   desc "Override deploy restart to not do anything"
   task :restart do
     #
   end
+end
+
+task :finalize_update, :except => { :no_release => true } do
+  run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
+  #link the themes, plugins, and config
+  run <<-CMD
+    rm -rf #{latest_release}/wordpress/wp-content/themes #{latest_release}/wordpress/wp-content/plugins &&
+    ln -s #{latest_release}/themes #{latest_release}/wordpress/wp-content/themes &&
+    ln -s #{latest_release}/plugins #{latest_release}/wordpress/wp-content/plugins &&
+    ln -s #{latest_release}/config/wp-config.php #{latest_release}/wordpress/wp-config.php
+  CMD
 end
